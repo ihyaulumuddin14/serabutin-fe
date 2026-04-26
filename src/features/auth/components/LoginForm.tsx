750982adee88c10aa0346fe71cd6aaeb373bc9ee
Ihya' Ulumuddin <pebehakun45@gmail.com>
2026-04-26 16:10:50 +0700

    refactor(auth): refactor AuthCard as a reusable component for auth layout

--- src/features/auth/components/LoginForm.tsx
+++ src/features/auth/components/LoginForm.tsx
@@ -1,11 +1,5 @@
 import { Button } from "@/shared/components/ui/button";
 import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from "@/shared/components/ui/card";
-import {
   Field,
   FieldDescription,
   FieldGroup,
@@ -15,64 +9,66 @@ import {
 import { Input } from "@/shared/components/ui/input";
 import { LockKeyhole, Mail } from "lucide-react";
 import { Link } from "react-router";
+import { AuthCard } from "./AuthCard";
 
 export default function LoginForm() {
   return (
-    <Card className="w-full max-w-110 p-5 sm:p-9.25 flex flex-col ">
-      <CardHeader className="text-center p-0!">
-        <CardTitle className="flex flex-col gap-3">
-          {/* title */}
-          <header className="w-full">
-            <h1 className="text-xl sm:text-[26px] tracking-[3.64px] font-inter font-bold text-foreground">
+    <AuthCard
+      title="Masuk"
+      description="Masuk ke akun anda"
+    >
+      <form>
+        <FieldGroup>
+          <Field>
+            <FieldLabel
+              icon={<Mail size={16} />}
+              htmlFor="email"
+            >
+              Email
+            </FieldLabel>
+            <Input
+              id="email"
+              type="email"
+              placeholder="contoh@email.com"
+              required
+            />
+          </Field>
+          <Field>
+            <FieldLabel
+              icon={<LockKeyhole size={16} />}
+              htmlFor="password"
+            >
+              Password
+            </FieldLabel>
+            <Input
+              id="password"
+              type="password"
+              placeholder="Minimal 8 karakter"
+              required
+            />
+          </Field>
+
+          <FieldSeparator />
+
+          <Field>
+            <Button
+              size={"lg"}
+              type="submit"
+            >
               Masuk
-            </h1>
-            <p className="font-normal">Masuk ke akun anda</p>
-          </header>
-        </CardTitle>
-      </CardHeader>
-      <CardContent className="p-0!">
-        <form>
-          <FieldGroup>
-            <Field>
-              <FieldLabel
-                icon={<Mail size={16} />}
-                htmlFor="email"
+            </Button>
+            <FieldDescription className="text-center">
+              Belum punya akun?{" "}
+              <Link
+                to="/register"
+                className="text-primary font-semibold no-underline!"
               >
-                Email
-              </FieldLabel>
-              <Input
-                id="email"
-                type="email"
-                placeholder="contoh@email.com"
-                required
-              />
-            </Field>
-            <Field>
-              <FieldLabel
-                icon={<LockKeyhole size={16} />}
-                htmlFor="password"
-              >
-                Password
-              </FieldLabel>
-              <Input
-                id="password"
-                type="password"
-                placeholder="Minimal 8 karakter"
-                required
-              />
-            </Field>
-
-            <FieldSeparator />
-
-            <Field>
-              <Button size={"lg"} type="submit">Masuk</Button>
-              <FieldDescription className="text-center">
-                Belum punya akun?{" "} <Link to="/register" className="text-primary font-semibold no-underline!">Registrasi</Link>
-              </FieldDescription>
-            </Field>
-          </FieldGroup>
-        </form>
-      </CardContent>
-    </Card>
+                Registrasi
+              </Link>
+            </FieldDescription>
+          </Field>
+        </FieldGroup>
+      </form>
+    </AuthCard>
   );
 }
--- src/features/auth/components/RegisterForm.tsx
+++ src/features/auth/components/RegisterForm.tsx
@@ -1,11 +1,5 @@
 import { Button } from "@/shared/components/ui/button";
 import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from "@/shared/components/ui/card";
-import {
   Field,
   FieldDescription,
   FieldGroup,
@@ -16,89 +10,91 @@ import { Input } from "@/shared/components/ui/input";
 import { Tabs, TabsList, TabsTrigger } from "@/shared/components/ui/tabs";
 import { LockKeyhole, Mail, UserRound } from "lucide-react";
 import { Link } from "react-router";
+import { AuthCard } from "./AuthCard";
 
 export default function RegisterForm() {
   return (
-    <Card className="w-full h-full sm:h-fit max-w-110 p-5 pt-16 sm:p-9.25 flex flex-col ">
-      <CardHeader className="text-center p-0!">
-        <CardTitle className="flex flex-col gap-3">
-          {/* title */}
-          <header className="w-full">
-            <h1 className="text-xl sm:text-[26px] tracking-[3.64px] font-inter font-bold text-foreground">
-              Registrasi
-            </h1>
-            <p className="font-normal">Buat akun baru dan mulai sekarang</p>
-          </header>
-        </CardTitle>
-      </CardHeader>
-      <CardContent className="p-0!">
-        <form>
-          <FieldGroup>
-            <Field>
-              <FieldLabel className="uppercase text-secondary-foreground">
-                Pilih Peran
-              </FieldLabel>
-              <Tabs defaultValue="client">
-                <TabsList className="w-full">
-                  <TabsTrigger value="client">Klien</TabsTrigger>
-                  <TabsTrigger value="worker">Pekerja</TabsTrigger>
-                </TabsList>
-              </Tabs>
-            </Field>
-            <Field>
-              <FieldLabel
-                icon={<Mail size={16} />}
-                htmlFor="email"
-              >
-                Email
-              </FieldLabel>
-              <Input
-                id="email"
-                type="email"
-                placeholder="contoh@email.com"
-                required
-              />
-            </Field>
-            <Field>
-              <FieldLabel
-                icon={<UserRound size={16} />}
-                htmlFor="username"
-              >
-                Nama Pengguna
-              </FieldLabel>
-              <Input
-                id="username"
-                type="text"
-                placeholder="Nama pengguna"
-                required
-              />
-            </Field>
-            <Field>
-              <FieldLabel
-                icon={<LockKeyhole size={16} />}
-                htmlFor="password"
-              >
-                Password
-              </FieldLabel>
-              <Input
-                id="password"
-                type="password"
-                placeholder="Minimal 8 karakter"
-                required
-              />
-            </Field>
+    <AuthCard
+      title="Registrasi"
+      description="Buat akun baru dan mulai sekarang"
+    >
+      <form>
+        <FieldGroup>
+          <Field>
+            <FieldLabel className="uppercase text-secondary-foreground">
+              Pilih Peran
+            </FieldLabel>
+            <Tabs defaultValue="client">
+              <TabsList className="w-full">
+                <TabsTrigger value="client">Klien</TabsTrigger>
+                <TabsTrigger value="worker">Pekerja</TabsTrigger>
+              </TabsList>
+            </Tabs>
+          </Field>
+          <Field>
+            <FieldLabel
+              icon={<Mail size={16} />}
+              htmlFor="email"
+            >
+              Email
+            </FieldLabel>
+            <Input
+              id="email"
+              type="email"
+              placeholder="contoh@email.com"
+              required
+            />
+          </Field>
+          <Field>
+            <FieldLabel
+              icon={<UserRound size={16} />}
+              htmlFor="username"
+            >
+              Nama Pengguna
+            </FieldLabel>
+            <Input
+              id="username"
+              type="text"
+              placeholder="Nama pengguna"
+              required
+            />
+          </Field>
+          <Field>
+            <FieldLabel
+              icon={<LockKeyhole size={16} />}
+              htmlFor="password"
+            >
+              Password
+            </FieldLabel>
+            <Input
+              id="password"
+              type="password"
+              placeholder="Minimal 8 karakter"
+              required
+            />
+          </Field>
 
-            <FieldSeparator />
+          <FieldSeparator />
 
-            <Field>
-              <Button size={"lg"} type="submit">Daftar Sekarang</Button>
-              <FieldDescription className="text-center">
-                Sudah punya akun?{" "} <Link to="/login" className="text-primary font-semibold no-underline!">Masuk</Link>
-              </FieldDescription>
-            </Field>
-          </FieldGroup>
-        </form>
-      </CardContent>
-    </Card>
+          <Field>
+            <Button
+              size={"lg"}
+              type="submit"
+            >
+              Daftar Sekarang
+            </Button>
+            <FieldDescription className="text-center">
+              Sudah punya akun?{" "}
+              <Link
+                to="/login"
+                className="text-primary font-semibold no-underline!"
+              >
+                Masuk
+              </Link>
+            </FieldDescription>
+          </Field>
+        </FieldGroup>
+      </form>
+    </AuthCard>
   );
 }
