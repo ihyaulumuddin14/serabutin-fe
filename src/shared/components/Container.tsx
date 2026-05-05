2d2349c5527762217c4178be3e16a3af3f7b453f
Ihya' Ulumuddin <pebehakun45@gmail.com>
2026-05-05 09:20:15 +0700

    style(responsive): update container padding and create responsiveness hooks (isMobile)

--- src/shared/components/Container.tsx
+++ src/shared/components/Container.tsx
@@ -1,6 +1,6 @@
 const Container = ({ children }: { children: React.ReactNode }) => {
   return (
-    <section className="w-full h-fit max-w-275 mx-auto px-4 py-6">{children}</section>
+    <section className="w-full h-fit max-w-275 mx-auto p-4 sm:p-6">{children}</section>
   );
 };
 
