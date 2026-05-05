9c08b8a9641a782e8c2552f26489b181081201e2
Ihya' Ulumuddin <pebehakun45@gmail.com>
2026-05-05 09:28:13 +0700

    chore(layout): create profile layout

--- src/shared/layouts/ProfileLayout.tsx
+++ src/shared/layouts/ProfileLayout.tsx
@@ -11,7 +11,7 @@ const ProfileLayout = () => {
 
       <main className="w-full h-[calc(100dvh-58px)] flex justify-center bg-[#F6F3EF] mt-14.5">
         <Container>
-          <div className="w-full grid grid-cols-1 md:grid-cols-[minmax(0,300px)_1fr] gap-6">
+          <div className="w-full grid grid-cols-1 md:grid-cols-[minmax(0,300px)_1fr] gap-4 sm:gap-6">
             <Outlet />
           </div>
         </Container>
