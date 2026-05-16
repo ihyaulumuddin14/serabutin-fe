9baa90f8caba3469c9c1686a8579803b2317001f
Ihya' Ulumuddin <pebehakun45@gmail.com>
2026-05-17 02:51:52 +0700

    feat(layout): update layout/nav/containers

--- src/shared/components/Container.tsx
+++ src/shared/components/Container.tsx
@@ -1,6 +1,6 @@
 const Container = ({ children }: { children: React.ReactNode }) => {
   return (
-    <section className="w-full h-fit max-w-275 mx-auto p-4 sm:p-6">{children}</section>
+    <section className="w-full h-fit max-w-300 mx-auto my-4 px-4 sm:px-0 sm:m-6">{children}</section>
   );
 };
 
--- src/shared/layouts/JobsLayout.tsx
+++ src/shared/layouts/JobsLayout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router'
 import NavbarMain from '../components/NavbarMain'
+import Container from '../components/Container'
 
 const JobsLayout = () => {
   return (
@@ -8,8 +9,12 @@ const JobsLayout = () => {
         <NavbarMain />
       </header>
 
-      <main className="w-full h-[calc(100dvh-58px)] flex justify-center items-center bg-[#F6F3EF] mt-14.5">
-        <Outlet />
+      <main className="w-full h-[calc(100dvh-58px)] flex justify-center bg-[#F6F3EF] mt-14.5">
+        <Container>
+          <div className="w-full grid grid-cols-1 md:grid-cols-[minmax(0,300px)_1fr] gap-4 sm:gap-6 relative">
+            <Outlet />
+          </div>
+        </Container>
       </main>
     </div>
   )
