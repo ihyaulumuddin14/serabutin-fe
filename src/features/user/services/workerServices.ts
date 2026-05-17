dd49f26be71992e27fac8c9c8d830c121ea7d562
Ihya' Ulumuddin <pebehakun45@gmail.com>
2026-05-17 23:18:26 +0700

    feat(bids): add bid creation flow

--- src/features/user/services/workerServices.ts
+++ src/features/user/services/workerServices.ts
@@ -1,8 +1,15 @@
 import privateApi from "@/shared/api/axiosInstance";
-import { toCamel } from "@/shared/lib/case";
+import { toCamel, toSnake } from "@/shared/lib/case";
 import type { ApiResponse, MetaPagination } from "@/shared/types/common.type";
-import type { Bid, JobAssignment } from "@/shared/types/entity.type";
+import type {
+  Bid,
+  JobAssignment,
+  Profile,
+  User,
+  WorkerAssignment,
+} from "@/shared/types/entity.type";
 import { ensureSuccess } from "./userServiceUtils";
+import type { BidCredentials } from "../schemas/bidSchemas";
 
 export const getWorkerAssignments = async (
   page: number = 1,
@@ -20,7 +27,7 @@ export const getWorkerAssignments = async (
   });
 
   const data = ensureSuccess(response, "Gagal mengambil data pekerjaan");
-  return toCamel(data) as ApiResponse<JobAssignment[]> & {
+  return toCamel(data) as ApiResponse<WorkerAssignment[]> & {
     meta: {
       currentPage: number;
       perPage: number;
@@ -46,7 +53,33 @@ export const getWorkerBids = async (
   if (response.data.status !== "success")
     throw new Error(response.data?.message || "Gagal mengambil data penawaran");
 
-  return response.data as ApiResponse<{ bid: Bid; job: JobAssignment }[]> & {
+  return response.data as ApiResponse<
+    {
+      bid: Bid;
+      job: JobAssignment;
+      client: Omit<User, "email" | "isVerified" | "isActive"> &
+        Partial<Pick<Profile, "phone">>;
+    }[]
+  > & {
     meta: MetaPagination;
   };
+};
+
+export const createBid = async (jobId: string, payload: BidCredentials) => {
+  const convertedPayload = toSnake(payload);
+
+  const response = await privateApi.post(
+    `/jobs/${jobId}/bids`,
+    convertedPayload,
+  );
+
+  const data = ensureSuccess(response, "Gagal mengirim penawaran");
+  return toCamel(data) as ApiResponse<Bid>;
+};
+
+export const cancelBid = async (bidId: string) => {
+  const response = await privateApi.delete(`/bids/${bidId}`);
+
+  const data = ensureSuccess(response, "Gagal membatalkan penawaran");
+  return toCamel(data) as ApiResponse<Bid>;
 };
